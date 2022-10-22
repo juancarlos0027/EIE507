e44ef355d6395b0250225779a223dd55cc8d809c
José Henríquez <jose@undercom.cl>
2022-10-22 00:26:59 -0300

    fix login

--- WebSocket_python/cgi-bin/login.py
+++ WebSocket_python/cgi-bin/login.py
@@ -6,12 +6,12 @@ import os
 cgitb.enable()
 
 form = cgi.FieldStorage()
-print(form)
 
 first_name = form.getvalue('name')
 last_name = form.getvalue('lastname')
-MAC = form.getvalue('MAC')
-if (MAC == "2C:54:6C:66:90:FF"):
+mac_address = form.getvalue('MAC')
+
+if (mac_address == "2C:54:6C:66:90:FF"):
     print("Content-Type:text/html;charset=utf-8")
     print("Content-type:text/html\r\n")
     print("<h1> Login OK </h1>")

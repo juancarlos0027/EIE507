3139c80387159c7101bae63e9a1eb4b5fa4fba8c
José Henríquez <jose@undercom.cl>
2022-11-05 20:11:44 -0300

    serial

--- proyecto_EIE507/RPi/get_serial_data.py
+++ proyecto_EIE507/RPi/get_serial_data.py
@@ -6,22 +6,29 @@ import time
 import datetime
 
 # Configuramos el puerto serial interno de la Raspberry Pi
-ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
+ser = serial.Serial('/dev/ttyAMA0',
+                    baudrate=9600,
+                    parity=serial.PARITY_NONE,
+                    stopbits=serial.STOPBITS_ONE,
+                    bytesize=serial.EIGHTBITS,
+                    timeout=1)
 
 # Entramos en un bucle infinito para leer los datos del puerto serial
 while True:
-    # Leemos los datos del puerto serial
-    data = ser.readline()
-    # Si los datos no son nulos, los imprimimos en pantalla
-    if data:
-        # Separa los datos por dos puntos
-        data = data.decode('utf-8').split(':')
-        
-        # Imprime los datos del sensor de CO
-        print('ID: ' + data[0])
-        print('CO: ' + data[1] + ' ppm')
+    try {
+        # Leemos los datos del puerto serial
+        data = ser.readline()
+        # Si los datos no son nulos, los imprimimos en pantalla
+        if data:
+            # Separa los datos por dos puntos
+            data = data.decode('utf-8').split(':')
+            
+            # Imprime los datos del sensor de CO
+            print('ID: ' + data[0])
+            print('CO: ' + data[1] + ' ppm')
 
-        # Imprime la fecha y hora
-        print(datetime.datetime.now())
-
-
+            # Imprime la fecha y hora
+            print(datetime.datetime.now())
+    } except KeyboardInterrupt:
+        print('Adquisición finalizada')
+        break

2dbef8bd1569a699fed2d4a3f664aee6f8e7dfa2
José Henríquez <jose@undercom.cl>
2022-11-27 21:25:39 -0300

    controller

--- proyecto_EIE507/RPi/APIserver/app/Http/Controllers/PublicDataController.php
+++ proyecto_EIE507/RPi/APIserver/app/Http/Controllers/PublicDataController.php
@@ -8,15 +8,20 @@ use Illuminate\Http\Request;
 class PublicDataController extends Controller
 {
     public function store(Request $request){
+        // the request is sent in a payload by the esp8266, so we need to decode it
+        $payload = json_decode($request->payload, true);
+
         if($sensor = Scan::create(
             [
-                'sensor_id' => $request->sensor_id,
-                'measure_unit_id' => $request->measure_unit_id,
-                'amount' => $request->amount
+                'sensor_id' => $payload['sensor_id'],
+                'measure_unit_id' => $payload['measure_unit_id'],
+                'amount' => $payload['amount']
             ]
-        ))        
+        )){
             return response()->json('OK', 200);
-        else
+        }
+        else{
             return response()->json(['error' => 'Error al guardar la información'], 500);
+        }
     }
 }

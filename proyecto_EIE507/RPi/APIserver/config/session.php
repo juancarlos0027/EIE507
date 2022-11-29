a3d5430233d0cd005ebd17685aae1f985db16ebe
José Henríquez <jose@undercom.cl>
2022-11-29 00:18:55 -0300

    session

--- proyecto_EIE507/RPi/APIserver/config/session.php
+++ proyecto_EIE507/RPi/APIserver/config/session.php
@@ -19,6 +19,7 @@ return [
     */
 
     'driver' => env('SESSION_DRIVER', 'database'),
+    
 
     /*
     |--------------------------------------------------------------------------
@@ -155,7 +156,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN'),
+    'domain' => env('SESSION_DOMAIN', null),
 
     /*
     |--------------------------------------------------------------------------
@@ -168,7 +169,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE'),
+    'secure' => env('SESSION_SECURE_COOKIE', false),
 
     /*
     |--------------------------------------------------------------------------
